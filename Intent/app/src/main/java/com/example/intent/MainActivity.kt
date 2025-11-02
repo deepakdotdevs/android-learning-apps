bcbf9e6f32a82f1c811a46f65d6ff21ae54ca81b
deepakdotdevs <deepak.06934@gmail.com>
2025-11-02 13:01:16 +0530

    Updated Version

--- Intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,8 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val editText = findViewById<EditText>(R.id.userInput)
-        val transfrButton = findViewById<Button>(R.id.saveexplicit)
+        val editText = findViewById<EditText>(R.id.userInput) // part of explicit
+        val transfrButton = findViewById<Button>(R.id.saveexplicit) // part of explicit
+        val shareButton = findViewById<Button>(R.id.shareimplicit) // part of implicit
+
+        // Explicit Intent
+
         transfrButton.setOnClickListener {
             val userInput = editText.text.toString()
             startActivity(Intent(this, NextScreen::class.java).putExtra("i", userInput))
@@ -28,6 +33,20 @@ class MainActivity : AppCompatActivity() {
 //            Intent.putExtra("data", text)
 //            startActivity(Intent)
 
+            // Implicit Intent
+
+        shareButton.setOnClickListener {
+            val userInput = editText.text.toString()
+            if(userInput.isNotEmpty()) {
+                val intent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, userInput)
+                }
+                startActivity(intent)
+            }else {
+                Toast.makeText(this, "Please write a Text Note, First", Toast.LENGTH_SHORT).show()
+            }
+            }
         }
     }
 }
--- Intent/app/src/main/java/com/example/intent/NextScreen.kt
+++ Intent/app/src/main/java/com/example/intent/NextScreen.kt
@@ -1,13 +1,18 @@
 package com.example.intent
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Button
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class NextScreen : AppCompatActivity() {
+    private lateinit var counterTextView : TextView
+    private var counter =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,5 +20,20 @@ class NextScreen : AppCompatActivity() {
         val RecivedData = intent.getStringExtra("i")
         val textview = findViewById<TextView>(R.id.textView)
         textview.text = RecivedData
+
+        counterTextView = findViewById<TextView>(R.id.textView2)
+        savedInstanceState?.let {
+            counter  = it.getInt("counter", 0)
+            counterTextView.text = counter.toString()
+        }
+        findViewById<Button>(R.id.button).setOnClickListener {
+            counter++
+            counterTextView.text= counter.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", counter)
     }
 }
